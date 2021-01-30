c7e790bf74df672ec6f5a663a97367e16b833307
Kaumudi Gupta <kaumudigpt@gmail.com>
2021-01-30 14:11:30 +0000

    Initial commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Kaumudi Gupta",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Kaumudi Gupta and I'm a Software Engineer!",
+  githubUrl: "https://github.com/kaumudigupta",
+  linkedinUrl: "https://www.linkedin.com/in/kaumudigupta",
+  cvLink: "https://drive.google.com/file/d/1_caXkFWjIOlGIUYmUYA34q3Ah8VzsmH8/view?usp=sharing",
+};
